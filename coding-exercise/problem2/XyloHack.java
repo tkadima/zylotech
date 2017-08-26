89f98e04acd80b6aa09296cf415acc52ace4839c
tkadima <kadima.t@husky.neu.edu>
2017-08-26 15:13:42 -0400

    completed the second coding exercise

--- coding-exercise/problem2/XyloHack.java
+++ coding-exercise/problem2/XyloHack.java
@@ -1,8 +1,39 @@
 package problem2;
 
+import java.util.Scanner;
+
 public class XyloHack {
 
+    public static String xyloHack(String input) {
+        // parse out the string from the input
+        String str = input.substring(1, input.indexOf('.') - 1);
+        // parse out the number given in the input
+        int num = Integer.parseInt(input.substring(input.indexOf('(') + 1, input.indexOf(')')));
+
+        if (num % 2 == 0) {
+            return str.toUpperCase();
+        } else {
+            return str.toLowerCase();
+        }
+    }
+
     public static void main (String [] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String [] hackArray = new String[n];
+        int i = 0;
+        while (i < n) {
+            String s = scanner.nextLine();
+            if (!s.isEmpty()) {
+                hackArray[i] = s;
+                i++;
+            }
+        }
+        // standard output, print out all the string
+        for (String input : hackArray) {
+            System.out.println(xyloHack(input));
+        }
+
     }
 }
+
